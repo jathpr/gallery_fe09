7d8d0330a61e432f2a18cba3fa55504dd490486b
Dzmitry Aliashkevich <dzmitrydon@gmail.com>
2021-09-23 21:21:09 +0300

    add user context

---
+++ src/store/userContext.js
@@ -0,0 +1,16 @@
+import { createContext, useState, useContext } from "react";
+import { listenToUserChange } from "../auth";
+const userContext = createContext();
+
+export const Provider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  listenToUserChange(setUser);
+
+  return <userContext.Provider value={user}>{children}</userContext.Provider>;
+};
+
+export const useUser = () => {
+  const currUser = useContext(userContext);
+  return currUser;
+};
