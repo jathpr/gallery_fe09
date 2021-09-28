bf90fd3b54b7e2cd7a9f3c90d0ae107fb78a5ed2
Alexei Kosykh <aleksei-kosykh@mail.ru>
2021-09-28 21:32:21 +0300

    imageFireStorage

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyDoUSwGE3wxsJphR4xz9pAXV7nb2Qb9l_8",
+  authDomain: "gallery-fe09.firebaseapp.com",
+  projectId: "gallery-fe09",
+  storageBucket: "gallery-fe09.appspot.com",
+  messagingSenderId: "749202424007",
+  appId: "1:749202424007:web:80c1814749a107eee87ee2",
+};
+
+export const app = initializeApp(firebaseConfig);
