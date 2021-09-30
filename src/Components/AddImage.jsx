5a8ef43558666c722f48e1076b38a673910cb984
criofox <79754985+criofox@users.noreply.github.com>
2021-09-30 21:26:09 +0300

    Add image adding

--- src/Components/AddImage.jsx
+++ src/Components/AddImage.jsx
@@ -2,13 +2,22 @@ import { uploadImage, downloadImage } from "../storage";
 import { useState, useEffect } from "react";
 export const AddImage = () => {
   const [url, setUrl] = useState();
+  const [value, setValue] = useState();
   useEffect(() => {
     downloadImage().then(setUrl);
   }, [downloadImage]);
+
   return (
     <>
-      <input onChange={(e) => uploadImage(e.target.files[0])} type="file" />
-      <img src={url} width="100" height="100" />;
+      <input onChange={(e) => setUrl(e.target.files[0])} type="file" />
+      <input onChange={(e) => setValue(e.target.value)} />
+      <button
+        onClick={() => {
+          uploadImage(url, value);
+        }}
+      >
+        Add
+      </button>
     </>
   );
 };
