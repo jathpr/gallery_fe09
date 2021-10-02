acf68c0ccf2ba24b05f4790861294cac70443685
criofox <79754985+criofox@users.noreply.github.com>
2021-10-02 16:37:39 +0300

    Add *Image Adding* (#30)
    * Add image adding
    
    * Delete useffect
    
    * Add functional that images adding from uid

--- src/Components/AddImage.jsx
+++ src/Components/AddImage.jsx
@@ -1,14 +1,22 @@
 import { uploadImage, downloadImage } from "../storage";
 import { useState, useEffect } from "react";
+import { useUser } from "../store/userContext";
 export const AddImage = () => {
   const [url, setUrl] = useState();
-  useEffect(() => {
-    downloadImage().then(setUrl);
-  }, [downloadImage]);
+  const [value, setValue] = useState();
+  const user = useUser();
+
   return (
     <>
-      <input onChange={(e) => uploadImage(e.target.files[0])} type="file" />
-      <img src={url} width="100" height="100" />;
+      <input onChange={(e) => setUrl(e.target.files[0])} type="file" />
+      <input onChange={(e) => setValue(e.target.value)} />
+      <button
+        onClick={() => {
+          uploadImage(url, value, user.uid);
+        }}
+      >
+        Add
+      </button>
     </>
   );
 };
